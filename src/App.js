67b16410e41a7ff9db5f6c7ffecf1876a1568484
Sofyan <70915827+scode03@users.noreply.github.com>
2022-09-14 12:05:27 +0700

    test redux

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
+import Example from "pages/Example";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/properties/:id" component={DetailsPage}></Route>
+        <Route path="/checkout" component={Checkout}></Route>
+        <Route path="/example" component={Example}></Route>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
